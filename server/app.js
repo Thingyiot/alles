b61582c12ab8f4d85694f0c6159ab0dcbe8c1627
Prashun  Javeri <scalewarrior@gmail.com>
2015-04-23 03:31:59 -0400

    express plans

--- server/app.js
+++ server/app.js
@@ -4,6 +4,44 @@
 var express = require('express');
 var app = express();
 
+
+//  authentication (get from primus) 
+//  require('./authentication')(app);
+
+// sessions
+// require('./sessions')(app);
+
+// security
+// require('./crytonode')(app);
+
+// ORM
+// require('./orm')(app);
+
+// Mongodb
+// require('./odm')(orm);
+
+// sequalize
+// require('./sequalize')(orm);
+
+//  cacheing 
+//  require('./cacheing')(app);
+
+// fixtures
+// require('./fixtures')(app);
+
+// transpots
+// require('./transpots')(app);
+
+// socketio
+// require('./socketio')(transpots);
+
+// amqp
+// require('./amqp')(transpots);
+
+// primus
+// require('./primus')(transpots);
+
+
  // Routing
 require('./routes')(app);
 
